41c538ef17f15eed65dae83380579425a6b1e093
shireen <shireen.gabut@gmail.com>
2026-02-06 23:13:23 +0900

    Update instr.py

--- instr.py
+++ instr.py
@@ -1,1 +1,14 @@
-# write a code for saving text instructions for the app
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+
+txt_hello = 'Welcome to the Health status detection program!'
+txt_next = 'Start'
+txt_instruction = ('This application allows you to use the Rufier test to make an initial diagnosis of your health.\n'
+                   'The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion.\n'
+                   'The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds; \n'
+                   'then, within 45 seconds, the subject performs 30 squats.\n'
+                   'When the exercise ends, the subject lies down and their pulse is measured again for the first 15 seconds\n'
+                   'and then for the last 15 seconds of the first minute of the recovery period.\n'
+                   'Important! If you feel unwell during the test (dizziness,\n'
+                   'tinnitus, shortness of breath, etc.), stop the test and consult a physician.' )
